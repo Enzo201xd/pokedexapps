5cf3121a5195737f16e0a1d0470d4a2be4df5fd6
Enzo <enzolabatte@gmail.com>
2024-04-10 08:07:11 -0300

    pages activity

--- server/src/pages/api/pokemon/index.json.ts
+++ server/src/pages/api/pokemon/index.json.ts
@@ -2,6 +2,7 @@ import type { APIContext, APIRoute } from "astro"
 import { addPokemon, getPokemonList, type Pokemon } from "../../../services/pokemon"
 
 export const GET: APIRoute = async (context) => {
+  const page = parseInt(context.url.searchParams.get('page') ?? '1', 10)
   const pokemonList = await getPokemonList();
   return new Response(JSON.stringify({pokemonList}), {
     headers: {
